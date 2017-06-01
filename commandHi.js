0c89a07165d982c420c9b7f8c3bc675f086bc74d
fancymans <lindarrin@gmail.com>
2017-06-01 02:03:04 -0700

    trying out discriminator for user greeting

--- commandHi.js
+++ commandHi.js
@@ -7,18 +7,18 @@ function getRandomIntInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-var responses = [
-    "Wsup.",
-    "Nigga who you is?",
-    "How hops it? <- lmao dafuq does that even mean?",
-    "Namaste.",
-    "Allô!",
-    "Si, fly.",
-    "**HI. MY NAME IS DAN.**\nI am definitely not Darrin, but Darrin is definitely me.",
-    "Greetings Lab Member " + getRandomIntInRange(100, 999) + ". _**El. Psy. Kongroo.**_"
-];
-
 module.exports = function(channel, author) {
+    var responses = [
+        "Wsup.",
+        "Nigga who you is?",
+        "How hops it? <- lmao dafuq does that even mean?",
+        "Namaste.",
+        "Allô!",
+        "Si, fly.",
+        "**HI. MY NAME IS DAN.**\nI am definitely not Darrin, but Darrin is definitely me.",
+        "Greetings Lab Member " + author.discriminator + ". _**El. Psy. Kongroo.**_"
+    ];
+
     var randNum = getRandomInt(responses.length);
     channel.sendMessage(responses[randNum]);
 }
